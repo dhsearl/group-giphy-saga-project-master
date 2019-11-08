4c7b3fee59c6a56d7b28c97b404b2dd6f7f746b8
Heather Walls <wallshkim@gmail.com>
2019-11-08 14:49:37 -0600

    Adds favorite_category table

--- database.sql
+++ database.sql
@@ -1,8 +1,3 @@
-CREATE DATABASE "giphy_search_favorites";
-
--- You'll need a table for storing each giphy image favorite
--- Each favorite image can be assigned 1 of the following categories as a Foreign Key
-
 -- Category table
 CREATE TABLE "category" (
     "id" SERIAL PRIMARY KEY,
@@ -16,9 +11,9 @@ VALUES ('uncategorized'), ('humor'), ('kids'), ('bizarre'), ('meme');
 -- Favorites table
 CREATE TABLE "favorites" (
 	"id" SERIAL PRIMARY KEY,
-	"url" TEXT,
-	"category_id" INT DEFAULT 1
+	"url" TEXT
 );
+
 
 INSERT INTO "favorites" ("url")
 VALUES ('https://media2.giphy.com/media/ZfUlRPE2zeZkQ/200.gif?cid=fddf64fb1179738dd48be64660a57207e757775169cdd1c8&rid=200.gif');
@@ -28,3 +23,13 @@ VALUES ('https://media2.giphy.com/media/ecOK5FrTNqX60/200.gif?cid=fddf64fb0450b9
 
 INSERT INTO "favorites" ("url")
 VALUES ('https://media0.giphy.com/media/K6PaXgRAzAVKE/200.gif?cid=fddf64fb457e1fb822f9f5b9ccd568337a0b75838b2eab6c&rid=200.gif');
+
+-- favorites_category
+CREATE TABLE "favorites_category" (
+	"id" SERIAL PRIMARY KEY,
+	"favorites_id" INT,
+	"category_id" INT
+);
+
+INSERT INTO "favorites_category" ("favorites_id", "category_id")
+VALUES (1, 2), (1, 3), (3, 5);
